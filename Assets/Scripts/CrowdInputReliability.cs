66b216886d00f0ab433032c3d6983d820c0cc8ae
KacperKenjiLesniak <kenjikacper@gmail.com>
2021-01-24 12:05:52 +0100

    CrowdInputReliability algorithm changes

--- Assets/Scripts/CrowdInputReliability.cs
+++ Assets/Scripts/CrowdInputReliability.cs
@@ -10,18 +10,23 @@ namespace DefaultNamespace
     {
         private IntPtr m_NativeObject = IntPtr.Zero;
         private int numberOfPlayers;
-        
-        public CrowdInputReliability(int numberOfPlayers, int numberOfCommands)
+
+        public CrowdInputReliability(
+            int numberOfPlayers,
+            int numberOfCommands,
+            float reliabilityCoefficient,
+            float agreementThreshold
+        )
         {
-            m_NativeObject = Internal_CreateCCrowdInputReliability(numberOfPlayers, numberOfCommands);
+            m_NativeObject = Internal_CreateCCrowdInputReliability(numberOfPlayers, numberOfCommands,reliabilityCoefficient, agreementThreshold);
             this.numberOfPlayers = numberOfPlayers;
         }
-        
+
         ~CrowdInputReliability()
-        { 
+        {
             Destroy();
         }
-        
+
         public void Destroy()
         {
             if (m_NativeObject != IntPtr.Zero)
@@ -30,7 +35,7 @@ namespace DefaultNamespace
                 m_NativeObject = IntPtr.Zero;
             }
         }
-        
+
         public int IssueCommands(int[] commands)
         {
             if (m_NativeObject == IntPtr.Zero)
@@ -38,27 +43,29 @@ namespace DefaultNamespace
             return Internal_IssueCommands(m_NativeObject, commands);
         }
 
-        public List<int> GetPlayerReliabilities()
+        public List<float> GetPlayerReliabilities()
         {
             var ptr = Internal_GetPlayerReliabilities(m_NativeObject);
-            var result = new int[numberOfPlayers];
+            var result = new float[numberOfPlayers];
             Marshal.Copy(ptr, result, 0, numberOfPlayers);
             return result.ToList();
         }
-        
+
         [DllImport("CROWDINPUTRELIABILITY", EntryPoint = "Internal_CreateCCrowdInputReliability")]
-        private static extern IntPtr Internal_CreateCCrowdInputReliability(int numberOfPlayers, int numberOfCommands);
-        
+        private static extern IntPtr Internal_CreateCCrowdInputReliability(
+            int numberOfPlayers,
+            int numberOfCommands,
+            float reliabilityCoefficient,
+            float agreementThreshold
+        );
+
         [DllImport("CROWDINPUTRELIABILITY", EntryPoint = "Internal_DestroyCCrowdInputReliability")]
         private static extern void Internal_DestroyCCrowdInputReliability(IntPtr obj);
 
-        [DllImport("CROWDINPUTRELIABILITY",  EntryPoint = "Internal_IssueCommands")]
+        [DllImport("CROWDINPUTRELIABILITY", EntryPoint = "Internal_IssueCommands")]
         private static extern int Internal_IssueCommands(IntPtr obj, int[] commands);
 
-        [DllImport("CROWDINPUTRELIABILITY",  EntryPoint = "Internal_GetPlayerReliabilities")]
+        [DllImport("CROWDINPUTRELIABILITY", EntryPoint = "Internal_GetPlayerReliabilities")]
         private static extern IntPtr Internal_GetPlayerReliabilities(IntPtr obj);
-
-        [DllImport("CROWDINPUTRELIABILITY",  EntryPoint = "releaseMemory")]
-        private static extern int releaseMemory(IntPtr obj);
     }
 }
--- Assets/Scripts/TestCaller.cs
+++ Assets/Scripts/TestCaller.cs
@@ -10,12 +10,21 @@ public class TestCaller : MonoBehaviour
 {
     void Start()
     {
-        var crowd = new CrowdInputReliability(3, 3);
-        
+        var crowd = new CrowdInputReliability(3, 3, 0.1f, 0.5f);
+        var crowd2 = new CrowdInputReliability(10, 3, 0.1f, 0.5f);
+
         // Display output of functions
         print(crowd.IssueCommands(new []{0, 0, 1}));
         print(crowd.IssueCommands(new []{1, 1, 0}));
+
         foreach (var reliability in crowd.GetPlayerReliabilities())
+        {
+            print(reliability);
+        }
+        
+        print("crowd2");
+        print(crowd2.IssueCommands(new []{0, 0, 0, 0 , 0, 1, 1, 1, 1, 2}));
+        foreach (var reliability in crowd2.GetPlayerReliabilities())
         {
             print(reliability);
         }
